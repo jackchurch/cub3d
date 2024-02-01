e8dd6488560bd27c83a2e7f847c4fe0f35a6622c
Reuben Kabzinski <kingofme50@hotmail.com>
2024-02-01 19:00:33 +1030

    Updated ray.c to reflect the new struct

--- src/ray.c
+++ src/ray.c
@@ -33,7 +33,7 @@ void	calculate_steps(float ray_angle, float *xstep, float *ystep, char axis)
 	}
 }
 
-void	cast_one_ray(float ray_angle, int stripId)
+void	cast_one_ray(t_game *game, float ray_angle, int stripId)
 {
 	t_wall_hit	horizontal;
 	t_wall_hit	vertical;
@@ -49,23 +49,23 @@ void	cast_one_ray(float ray_angle, int stripId)
 			vertical.wall_hit_x, vertical.wall_hit_y);
 	//printf("Vert Distance: %f\nHori Distance: %f\n", vertical.distance, horizontal.distance);
 	if (vertical.distance < horizontal.distance)
-		ray_cast(&vertical, stripId, ray_angle);
+		ray_cast(game, &vertical, stripId, ray_angle);
 	else
-		ray_cast(&horizontal, stripId, ray_angle);
+		ray_cast(game, &horizontal, stripId, ray_angle);
 }
 
-void	ray_cast(t_wall_hit *hit, int stripId, float ray_angle)
+void	ray_cast(t_game *game, t_wall_hit *hit, int stripId, float ray_angle)
 {
-	rays[stripId].distance = hit->distance;
-	rays[stripId].wall_hit_x = hit->wall_hit_x;
-	rays[stripId].wall_hit_y = hit->wall_hit_y;
-	rays[stripId].wall_hit_content = hit->wall_content;
-	rays[stripId].was_hit_vertical = hit->is_vertical;
-	rays[stripId].ray_angle = ray_angle;
-	rays[stripId].is_ray_facing_down = is_ray_facing_down(ray_angle);
-	rays[stripId].is_ray_facing_up = is_ray_facing_up(ray_angle);
-	rays[stripId].is_ray_facing_left = is_ray_facing_left(ray_angle);
-	rays[stripId].is_ray_facing_right = is_ray_facing_right(ray_angle);
+	game->rays[stripId].distance = hit->distance;
+	game->rays[stripId].wall_hit_x = hit->wall_hit_x;
+	game->rays[stripId].wall_hit_y = hit->wall_hit_y;
+	game->rays[stripId].wall_hit_content = hit->wall_content;
+	game->rays[stripId].was_hit_vertical = hit->is_vertical;
+	game->rays[stripId].ray_angle = ray_angle;
+	game->rays[stripId].is_ray_facing_down = is_ray_facing_down(ray_angle);
+	game->rays[stripId].is_ray_facing_up = is_ray_facing_up(ray_angle);
+	game->rays[stripId].is_ray_facing_left = is_ray_facing_left(ray_angle);
+	game->rays[stripId].is_ray_facing_right = is_ray_facing_right(ray_angle);
 }
 
 void	cast_all_rays(t_game *game)
@@ -77,7 +77,7 @@ void	cast_all_rays(t_game *game)
 	ray_angle = player.rotation_angle - FOV_ANGLE / 2;
 	while (strip_id < game->num_rays)
 	{
-		cast_one_ray(ray_angle, strip_id);
+		cast_one_ray(game, ray_angle, strip_id);
 		ray_angle += FOV_ANGLE / game->num_rays;
 		strip_id++;
 	}
@@ -93,8 +93,8 @@ void	render_rays(t_game *game)
 	{
 		line.x0 = player.x * MINIMAP_SCALE;
 		line.y0 = player.y * MINIMAP_SCALE;
-		line.x1 = rays[i].wall_hit_x * MINIMAP_SCALE;
-		line.y1 = rays[i].wall_hit_y * MINIMAP_SCALE;
+		line.x1 = game->rays[i].wall_hit_x * MINIMAP_SCALE;
+		line.y1 = game->rays[i].wall_hit_y * MINIMAP_SCALE;
 		line.color = 0x00FFC8D7;
 		draw_line(game, &line);
 		i++;
