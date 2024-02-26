401feccc5ba82e09a2d300768f535057be1b64a2
Reuben Kabzinski <kingofme50@hotmail.com>
2024-02-26 18:10:33 +1030

    Changed error messages from char ** to char *, woops

--- src/error.c
+++ src/error.c
@@ -12,17 +12,17 @@
 
 #include <stdio.h>
 
-void	err_v(char **str)
+void	err_v(char *str)
 {
 	printf("Error\n\t%s\n", str);
 }
 
-int	err_i(char **str)
+int	err_i(char *str)
 {
 	return (printf("Error\n\t%s\n", str));
 }
 
-void	*err_p(char **str)
+void	*err_p(char *str)
 {
 	printf("Error\n\t%s\n", str);
 	return (NULL);
